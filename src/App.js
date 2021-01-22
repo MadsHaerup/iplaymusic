a7a52b3808ee0481205c006b039bba6e493e8273
Mads Haerup <506094@rts-365.dk>
2021-01-22 18:00:25 +0100

    Secondary Navigation

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import Welcome from './pages/Welcome';
 import Featured from './pages/Featured';
 import Playlist from './pages/Playlist';
 import Categories from './pages/Categories';
+import SecondaryNav from './components/SecondaryNav';
 
 function App() {
 	return (
 		<Router>
 			<Route path="/" exact component={Welcome} />
 			<Route path="/login" component={Login} />
-			<Route path="/featured" component={Featured} />
-			<Route path="/playlist" component={Playlist} />
-			<Route path="/categories" component={Categories} />
+			<Route path="/featured">
+				<SecondaryNav />
+				<Featured />
+			</Route>
+			<Route path="/playlist">
+				<SecondaryNav />
+				<Playlist />
+			</Route>
+			<Route path="/categories">
+				<SecondaryNav />
+				<Categories />
+			</Route>
 		</Router>
 	);
 }
---
+++ src/components/SecondaryNav.js
@@ -0,0 +1,19 @@
+import { IoIosArrowBack } from 'react-icons/io';
+import { BsSearch } from 'react-icons/bs';
+import { useLocation } from 'react-router-dom';
+import './SecondaryNav.scss';
+const SecondaryNav = () => {
+	let location = useLocation();
+	return (
+		<div className="secondaryNav">
+			<button className="secondaryNav__arrow">
+				<IoIosArrowBack />
+			</button>
+			<p className="secondaryNav__text">{location.pathname.slice(1)}</p>
+			<button className="secondaryNav__search">
+				<BsSearch />
+			</button>
+		</div>
+	);
+};
+export default SecondaryNav;
